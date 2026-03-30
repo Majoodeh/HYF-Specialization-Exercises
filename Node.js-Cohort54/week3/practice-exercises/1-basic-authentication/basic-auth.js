9458a7731d3ebae1555372933c4477a1a152781f
Majoodeh <Majdhussein144@gmail.com>
2026-03-30 19:35:45 +0200

    Organize all HYF exercises into categorized folders

---
+++ Node.js-Cohort54/week3/practice-exercises/1-basic-authentication/basic-auth.js
@@ -0,0 +1,15 @@
+
+/**
+ * 2. Authentication
+ * 
+ * Using fetch, make an authenticated request to https://httpbin.org/basic-auth/admin/hvgX8KlVEa
+ * Print the response to the console. Use async-await and try/catch.
+ * 
+ * Hints:
+ * - for basic authentication the username and password need to be base64 encoded
+ */
+const testAuthentication = async () => {
+  // YOUR CODE GOES IN HERE
+}
+
+testAuthentication().catch(console.error);
